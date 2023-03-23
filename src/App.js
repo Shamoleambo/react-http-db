042578a34601fd6eb4c57c4245cac2ecaa29bfa9
Tiago Gomes <tidgomes@gmail.com>
2023-03-23 14:24:27 -0300

    create AddMovie component and its module.css file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react'
 
 import MoviesList from './components/MoviesList'
+import AddMovie from './components/AddMovie'
 import './App.css'
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
     setError(null)
 
     try {
-      const response = await fetch('https://swapi.dev/api/films')
+      const response = await fetch(
+        'https://react-academind-3054e-default-rtdb.firebaseio.com/movies.json'
+      )
       if (!response.ok) throw new Error('Something went wrong.')
 
       const data = await response.json()
@@ -33,6 +36,10 @@ function App() {
     setIsLoading(false)
   }, [])
 
+  function addMovieHandler(movie) {
+    console.log(movie)
+  }
+
   useEffect(() => {
     fetchMovieHandler()
   }, [fetchMovieHandler])
@@ -44,6 +51,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
